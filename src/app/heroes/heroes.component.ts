541755499e6f6809d4abe205492032c2d10bb8a4
ttmmnn <124642000+ttmmnn@users.noreply.github.com>
2023-10-09 15:20:10 +0900

    Update_observable

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -23,6 +23,6 @@ export class HeroesComponent {
   }
 
   getHeroes(): void {
-    this.heroes = this.heroService.getHeroes();
+    this.heroService.getHeroes().subscribe((heroes) => (this.heroes = heroes));
   }
 }
